d2f86adb122436c1e03357efcfbbb3501c69fdd1
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2021-06-29 19:13:33 +0530

    Add files via upload

---
+++ js/AddButtons.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent('add-buttons', {
+    init: function () {
+        var button1 = document.createElement('button')
+        button1.innerHTML = 'ORDER NOW'
+        button1.setAttribute('id', 'order-button')
+        button1.setAttribute('class', 'buttons')
+
+        var button2 = document.createElement('button')
+        button2.innerHTML = 'ORDER SUMMARY'
+        button2.setAttribute('id', 'order-summary-button')
+        button2.setAttribute('class', 'buttons')
+
+        var button3 = document.createElement('button')
+        button3.innerHTML = 'RATE NOW'
+        button3.setAttribute('id', 'rating-button')
+        button3.setAttribute('class', 'buttons')
+
+        var buttonDiv = document.getElementById('button-div')
+        buttonDiv.appendChild(button2)
+        buttonDiv.appendChild(button1)
+        buttonDiv.appendChild(button3)
+    },
+});
